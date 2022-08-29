42f8cf94a81828883ae7656820731299aaf05b05
mariasuarez <mariasuarezw@gmail.com>
2022-08-29 12:42:35 -0500

    Update tablas_miniproyecto.sql
    todas las tablas

--- tablas_miniproyecto.sql
+++ tablas_miniproyecto.sql
@@ -1,7 +1,7 @@
 --Tabla Usuario
 
 CREATE TABLE usuario(
-	idUsuario serial primary key not null unique ,
+	idUsuario numeric(10) primary key not null,
 	nombre char(50) not null,
 	contrasena char(20) not null,
 	tipo bool not null); 
@@ -9,8 +9,32 @@ CREATE TABLE usuario(
 --Tabla Producto
 
 CREATE TABLE producto(
-	codigoBarra numeric(15) primary key unique,
+	codigoBarra numeric(15) primary key,
 	nombre char(20) not null,
 	precio numeric(8) not null,
 	cantidad numeric(5) not null,
 	capacidad numeric(5) not null);
+
+--Tabla Venta
+CREATE TABLE venta(
+	idVenta numeric(10),
+	idProducto numeric(15),
+	cantidad numeric(4),
+	primary key(idVenta, idProducto),
+		foreign key(idProducto)
+			references producto(codigoBarra));
+	
+--Tabla registros de ventas
+CREATE TABLE registroVenta(
+	idRegistro numeric(10) primary key not null,
+	usuario numeric(11) not null,
+	idCliente_venta numeric(11),
+	infoVenta char(200),
+	fecha date not null,
+	foreign key(usuario)
+		references usuario(idUsuario));
+
+--Tabla categoria productos
+CREATE TABLE categoria(
+	codigo numeric(8) primary key,
+	nombre char(20) not null);
