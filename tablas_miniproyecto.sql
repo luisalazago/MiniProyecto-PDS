92442ae16ee05accf03870f8fa1678c8f95d210a
Guido <guidoesalazar@gmail.com>
2022-08-29 13:00:32 -0500

    Arreglos creación de tablas

--- tablas_miniproyecto.sql
+++ tablas_miniproyecto.sql
@@ -1,40 +1,37 @@
 --Tabla Usuario
-
 CREATE TABLE usuario(
-	idUsuario numeric(10) primary key not null,
+	idUsuario numeric(10) primary key,
 	nombre char(50) not null,
 	contrasena char(20) not null,
 	tipo bool not null); 
-	
---Tabla Producto
-
-CREATE TABLE producto(
-	codigoBarra numeric(15) primary key,
-	nombre char(20) not null,
-	precio numeric(8) not null,
-	cantidad numeric(5) not null,
-	capacidad numeric(5) not null);
-
---Tabla Venta
-CREATE TABLE venta(
-	idVenta numeric(10),
-	idProducto numeric(15),
-	cantidad numeric(4),
-	primary key(idVenta, idProducto),
-		foreign key(idProducto)
-			references producto(codigoBarra));
-	
---Tabla registros de ventas
-CREATE TABLE registroVenta(
-	idRegistro numeric(10) primary key not null,
-	usuario numeric(11) not null,
-	idCliente_venta numeric(11),
-	infoVenta char(200),
-	fecha date not null,
-	foreign key(usuario)
-		references usuario(idUsuario));
 
 --Tabla categoria productos
 CREATE TABLE categoria(
 	codigo numeric(8) primary key,
 	nombre char(20) not null);
+
+--Tabla Producto
+CREATE TABLE producto(
+	codigoBarra numeric(15) primary key,
+	nombre char(20) not null,
+	precio numeric(8) not null,
+	cantidad numeric(5) not null,
+	capacidad numeric(5) not null.
+	tipo numeric(8) references categoria(codigo) not null);
+
+--Tabla Venta
+CREATE TABLE venta(
+	idVenta numeric(10) references registroVenta(idRegistro),
+	idProducto numeric(15) references producto(codigoBarra),
+	cantidad numeric(5),
+	primary key(idVenta, idProducto));
+	
+--Tabla registros de ventas
+CREATE TABLE registroVenta(
+	idRegistro numeric(10) primary key,
+	usuario numeric(11) not null,
+	idCliente_venta numeric(11),
+	infoVenta char(200),
+	fecha date not null,
+	foreign key(usuario) references usuario(idUsuario));
+
