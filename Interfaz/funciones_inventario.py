523c46c41bce7ff5ea042492dcd00aa52c89604f
Luis <luisalazago@gmail.com>
2022-08-30 23:30:10 -0500

    Inventario test

--- Interfaz/funciones_inventario.py
+++ Interfaz/funciones_inventario.py
@@ -24,15 +24,40 @@ def revisarInventario():
     print("===================================================")
     print("Bienvenido al modulo del Inventario. A continuacion digite el producto a evaluar.")
     print("===================================================")
-    idProducto = int(input("Digite el id del producto: "))
-    nombre, cantidad, capacidad, precio = obtenerProducto(idProducto)
+    print("Que desea hacer a continuacion: ")
+    print("1) Revisar todo el inventario.")
+    print("2) Revisar el inventario de un solo proudcto.")
+    des = int(input("Digite la opcion: "))
+    while(des > 2 or des < 1):
+        print("")
+        print("Por favor digite un numero correcto.")
+        des = int(input("Digite la opcion: "))
     print("")
-    print("===================================================")
-    print("Nombre del producto: ".format(nombre))
-    print("Cantidad del producto: ".format(cantidad))
-    print("Capacidad maxima del producto: ".format(capacidad))
-    print("Precio del producto: ".format(precio))
-    print("===================================================")
+    
+    if(des == 2):
+        idProducto = int(input("Digite el id del producto: "))
+        lista = obtenerProducto(idProducto)
+        print("")
+        print("===================================================")
+        print("Nombre del producto: ".format(lista[0]))
+        print("Cantidad del producto: ".format(lista[1]))
+        print("Capacidad maxima del producto: ".format(lista[2]))
+        print("Precio del producto: ".format(lista[3]))
+        print("===================================================")
+    else:
+        inventario = obtenerInventario()
+        print("===================================================")
+        print("Inventario disponible de la empresa XYZ")
+        for lista in inventario:
+            print("#####################################")
+            print("Codigo del producto: ".format(lista[0]))
+            print("Nombre del producto: ".format(lista[1]))
+            print("Precio del producto: ".format(lista[2]))
+            print("Cantidad disponible del producto: ".format(lista[3]))
+            print("Tipo de producto: ".format(lista[4]))
+            print("#####################################")
+        print("===================================================")
+             
 
 if __name__ == "__main__":
     obtenerInventario()
