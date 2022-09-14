3f8f2922564741531fef57b0ff2e2bb00c3164e1
Guido <guidoesalazar@gmail.com>
2022-09-14 10:02:37 -0500

    IsAdmin

--- Funciones/usuario.py
+++ Funciones/usuario.py
@@ -7,5 +7,10 @@ def verificar_contrasena(cc, password):
     if len(data) != 0 and data[0][0].strip() == str(password).strip(): ans = True 
     return ans
 
+def is_admin(cc):
+    sql = "SELECT tipo FROM usuario WHERE idUsuario = %s"
+    data = select(sql, [cc])
+    return data[0][0]
+
 if __name__ == "__main__":
-    print(verificar_contrasena(1000000000, "1234"))
+    is_admin(1000000000)
